d58198d90952fdf013ae5f6f9be795ef47006354
Krish Ahlawat <krishahlawat2520@gmail.com>
2025-01-29 22:27:39 +0530

    Added analytics

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,9 +6,12 @@ import { Navbar } from "./navbar";
 import { ProjectsSection } from "./(project)/projects-section";
 import { SkillsSection } from "./(skills)/skills-section";
 import { ContactSection } from "./(contact)/contact-section";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function Home() {
   return (
+    <>
+    <GoogleAnalytics trackPageViews />
     <div>
       <Navbar />
       <HeroHighlight />
@@ -17,5 +20,6 @@ export default function Home() {
       <SkillsSection />
       <ContactSection />
     </div>
+    </>
   );
 }
